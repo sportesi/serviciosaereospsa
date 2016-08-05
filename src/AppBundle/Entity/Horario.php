0b9b521da1ff1f975dc2a9ecbdacb5f08b3eaef9
Sebastian Portesi <sebastian@proyectiva.com>
2016-08-05 00:48:58 -0300

    .

---
+++ src/AppBundle/Entity/Horario.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace AppBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * Horario
+ *
+ * @ORM\Table(name="horario")
+ * @ORM\Entity(repositoryClass="AppBundle\Repository\HorarioRepository")
+ */
+class Horario
+{
+    /**
+     * @var int
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+
+    /**
+     * @var int
+     *
+     * @ORM\Column(name="hora", type="integer")
+     */
+    private $hora;
+
+
+    /**
+     * Get id
+     *
+     * @return int
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set hora
+     *
+     * @param integer $hora
+     *
+     * @return Horario
+     */
+    public function setHora($hora)
+    {
+        $this->hora = $hora;
+
+        return $this;
+    }
+
+    /**
+     * Get hora
+     *
+     * @return int
+     */
+    public function getHora()
+    {
+        return $this->hora;
+    }
+}
+
