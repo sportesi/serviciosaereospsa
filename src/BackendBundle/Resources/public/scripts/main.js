5a950dbe9637c48688216e895eac435ca4f82f99
sportesi <sebastian.nahuel.portesi@gmail.com>
2016-08-08 16:50:56 -0300

    .

--- src/BackendBundle/Resources/public/scripts/main.js
+++ src/BackendBundle/Resources/public/scripts/main.js
@@ -1,3 +1,13 @@
 String.prototype.capitalizeFirstLetter = function() {
     return this.charAt(0).toUpperCase() + this.slice(1);
 }
+
+
+
+/* Calendar logic */
+
+function InitCalendar() {
+	$('td.clickable').on('click', function(){
+		console.log(this);
+	})
+}
