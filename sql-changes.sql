049a1186c2edf598e144751a7a122c743edfecf1
Sebastian Portesi <Sebastian Portesi>
2016-10-01 15:35:06 -0300

    .

--- sql-changes.sql
+++ sql-changes.sql
@@ -1,4 +1,4 @@
 -- 01/10/2016
 -- ALTER TABLE avion ADD avion_order INT DEFAULT NULL;
-ALTER TABLE avion ADD fecha_fuera_servicio_hasta DATE DEFAULT NULL;
-ALTER TABLE avion DROP fecha_fuera_servicio, DROP fecha_fuera_servicio_hasta, CHANGE desde_fuera_servicio desde_fuera_servicio DATETIME DEFAULT NULL, CHANGE hasta_fuera_servicio hasta_fuera_servicio DATETIME DEFAULT NULL;
+-- ALTER TABLE avion ADD fecha_fuera_servicio_hasta DATE DEFAULT NULL;
+-- ALTER TABLE avion DROP fecha_fuera_servicio, DROP fecha_fuera_servicio_hasta, CHANGE desde_fuera_servicio desde_fuera_servicio DATETIME DEFAULT NULL, CHANGE hasta_fuera_servicio hasta_fuera_servicio DATETIME DEFAULT NULL;
