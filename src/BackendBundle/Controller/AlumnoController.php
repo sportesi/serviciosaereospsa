97abb7c6050ff566008a39d0519e9405cf05fe20
Sebastian Portesi <sebastian@proyectiva.com>
2016-10-23 15:39:10 -0300

    .

--- src/BackendBundle/Controller/AlumnoController.php
+++ src/BackendBundle/Controller/AlumnoController.php
@@ -2,9 +2,12 @@
 
 namespace BackendBundle\Controller;
 
-use Symfony\Bundle\FrameworkBundle\Controller\Controller;
-use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use AppBundle\Entity\Alumno;
+use AppBundle\Form\AlumnoType;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
 
 class AlumnoController extends Controller {
 
@@ -22,6 +25,7 @@ class AlumnoController extends Controller {
 
     /**
      * @Route("/alumno/edit/{id}", defaults = { "id" = 0 }, name = "BackendAlumnoEdit")
+     * @Method({"GET"})
      * @param integer $id
      * @return View
      */
@@ -30,14 +34,43 @@ class AlumnoController extends Controller {
         if ($id != 0) {
             $alumnoToEdit = $em->getRepository("AppBundle:Alumno")->find($id);
         } else {
-            $alumnoToEdit = new \AppBundle\Entity\Alumno();
+            $alumnoToEdit = new Alumno();
         }
-        
-        $alumnoForm = $this->createForm(\AppBundle\Form\AlumnoType::class, $alumnoToEdit);
+
+        $alumnoForm = $this->createForm(AlumnoType::class, $alumnoToEdit);
         $pageParameters = array(
             'form' => $alumnoForm->createView(),
         );
         return $this->render("BackendBundle:AlumnoViews:edit.html.twig", $pageParameters);
+    }
+
+    /**
+     * @Route("/alumno/edit/{id}", defaults = { "id" = 0 }, name = "BackendAlumnoSave")
+     * @Method({"POST"})
+     * @param Request $request
+     * @return type
+     */
+    function saveAction(Request $request, $id) {
+        $em = $this->getDoctrine()->getManager();
+        $rp = $em->getRepository("AppBundle:Alumno");
+        if ($id != 0) {
+            $alumno = $rp->find($id);
+        } else {
+            $alumno = new Alumno();
+        }
+
+        $form = $this->createForm(AlumnoType::class, $alumno);
+        $form->handleRequest($request);
+        if ($form->isValid()) {
+            $em->persist($alumno);
+            $em->flush();
+            return $this->redirectToRoute("BackendAlumnoEdit", array("id" => $alumno->getId()));
+        } else {
+            $pageParameters = array(
+                'form' => $form->createView(),
+            );
+            return $this->render("BackendBundle:AlumnoViews:edit.html.twig", $pageParameters);
+        }
     }
 
 }
