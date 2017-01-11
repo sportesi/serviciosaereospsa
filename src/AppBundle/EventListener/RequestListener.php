b8e2d16eaccd71435f22636a5da3ab6eaa856fb5
Sebastian Portesi <sebastian.nahuel.portesi@gmail.com>
2017-01-11 14:47:55 -0300

    - Temporary commit.

---
+++ src/AppBundle/EventListener/RequestListener.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: sebastian
+ * Date: 11/01/17
+ * Time: 14:45
+ */
+
+namespace AppBundle\EventListener;
+
+
+class RequestListener
+{
+    public function onKernelRequest()
+    {
+
+    }
+}
