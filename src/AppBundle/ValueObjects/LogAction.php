952ff9ae52d5bc74c445e6e06f521e1858bae08c
Sebastian Portesi <sebastian.nahuel.portesi@gmail.com>
2017-01-13 14:12:31 -0300

    - Temporary commit.

---
+++ src/AppBundle/ValueObjects/LogAction.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: sebastian
+ * Date: 13/01/17
+ * Time: 14:09
+ */
+
+namespace AppBundle\ValueObjects;
+
+
+class LogAction
+{
+    const turno = 'apa';
+}
