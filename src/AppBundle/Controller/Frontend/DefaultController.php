26a7d4435c6f5d1ca22324b29311404447adf4b6
Sebastian Portesi <sebastian.nahuel.portesi@gmail.com>
2017-01-18 18:25:35 -0300

    Re-arranged frontend styles and views.

--- src/AppBundle/Controller/Frontend/DefaultController.php
+++ src/AppBundle/Controller/Frontend/DefaultController.php
@@ -31,6 +31,6 @@ class DefaultController extends Controller
      */
     public function indexAction()
     {
-        return $this->render('FrontendBundle:Default:index.html.twig');
+        return $this->render('Frontend/Default/index.html.twig');
     }
 }
