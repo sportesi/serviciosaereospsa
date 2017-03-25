7523548233e353871fd6a29187df55b383b0e69a
Sebastian Portesi <sebastian.nahuel.portesi@gmail.com>
2017-03-25 11:42:01 -0300

    Maintenace Page

---
+++ public_html/maintenance.php
@@ -0,0 +1,49 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <title>Servicios A&eacute;reos PSA</title>
+        <meta charset="utf-8">
+        <meta name="description" content="servicios aereos psa escuela de vuelo"/>
+
+        <link rel="stylesheet" type="text/css" href="css/coming-soon.css">
+        <link rel="stylesheet" type="text/css" href="css/app.min.css">
+
+        <script type="text/javascript" src="js/app.min.js"></script>
+    </head>
+    <body>
+
+        <section class="content wrapper" id="container">
+
+            <h1>Pr&oacute;ximamente Disponible</h1>
+            <p class="description">
+                Si actualemente sos piloto o alumno de PSA
+                <br />
+                comunicate con nosotros.
+            </p>
+            <p>
+                Email: <a class="white-link" href="mailto:escuelapsa@gmail.com">escuelapsa@gmail.com</a>
+                <br />
+                Telefono: 11-6258-8919
+            </p>
+        </section>
+
+        <div class="back_img"></div>
+
+    </body>
+    <script>
+        (function (i, s, o, g, r, a, m) {
+            i['GoogleAnalyticsObject'] = r;
+            i[r] = i[r] || function () {
+                    (i[r].q = i[r].q || []).push(arguments)
+                }, i[r].l = 1 * new Date();
+            a = s.createElement(o),
+                m = s.getElementsByTagName(o)[0];
+            a.async = 1;
+            a.src = g;
+            m.parentNode.insertBefore(a, m)
+        })(window, document, 'script', 'https://www.google-analytics.com/analytics.js', 'ga');
+
+        ga('create', 'UA-92177624-1', 'auto');
+        ga('send', 'pageview');
+    </script>
+</html>
